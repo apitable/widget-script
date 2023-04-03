60034843a860176aa0d30dcd2e715a2dd89422f5
Sky <huangchangpeng@vikadata.com>
2023-04-03 19:12:02 +0800

    feat: initialization

---
+++ src/render_components/output.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Output } from './output.interface';
+import { Markdown, Table, Text } from './components';
+import { IContentWindow, RenderBase } from './render_base';
+
+export class OutputClass extends RenderBase implements Output {
+  constructor(window: IContentWindow) {
+    super(window)
+  }
+
+  public text(name: string) {
+    this.renderComponent(
+      <Text name={name} />
+    );
+  }
+
+  public markdown(source: string) {
+    this.renderComponent(
+      <Markdown 
+        source={source}
+      />
+    );
+  }
+
+  public table(data: (string | number | Object)[]) {
+    const props = { data };
+    this.renderComponent(
+      <Table {...props as any} />
+    );
+  }
+
+  public clear() {
+    let child = this.container.lastElementChild;  
+    while (child) { 
+      this.container.removeChild(child); 
+      child = this.container.lastElementChild; 
+    }
+  }
+}
